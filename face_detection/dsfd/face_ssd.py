547f949cd530369d7d19b32ead3d5aa9c5cc07ce
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-12-12 23:09:43 +0100

    Update face_ssd.py

--- face_detection/dsfd/face_ssd.py
+++ face_detection/dsfd/face_ssd.py
@@ -284,6 +284,18 @@ def pa_multibox(output_channels, mbox_cfg, num_classes):
             DeepHeadModule(input_channels, mbox_cfg[k] * (2+conf_output))]
     return (loc_layers, conf_layers)
 
+def init_priors(self, feature_maps, image_size):
+
+    # Hacky key system, but works....
+    key = ".".join([str(item) for i in range(len(feature_maps)) for item in feature_maps[i]]) + \
+          "," + ".".join([str(_) for _ in image_size])
+    if key in self.prior_cache:
+        return self.prior_cache[key].clone()
+
+    priorbox = PriorBox(self.cfg, image_size, feature_maps)
+    prior = priorbox.forward()
+    self.prior_cache[key] = prior.clone()
+    return prior
 
 
 class SSD_TensorRT(nn.Module):
@@ -385,3 +397,36 @@ class SSD_TensorRT(nn.Module):
         conv3_3_x = lfpn1
 
         return self.cpm3_3(conv3_3_x), self.cpm4_3(conv4_3_x), self.cpm5_3(conv5_3_x), self.cpm7(fc7_x), self.cpm6_2(conv6_2_x), self.cpm7_2(conv7_2_x)
+
+    def get_boxes_scores(sources, loc, conf, mio_module, cfg, init_priors, num_classes):
+        featuremap_size = []
+        for (x, l, c) in zip(sources, self.loc, self.conf):
+            featuremap_size.append([x.shape[2], x.shape[3]])
+            loc.append(l(x).permute(0, 2, 3, 1).contiguous())
+
+            # Max in out
+            len_conf = len(conf)
+            out = self.mio_module(c(x), len_conf)
+
+            conf.append(out.permute(0, 2, 3, 1).contiguous())
+        # Progressive Anchor
+        mbox_num = self.cfg['mbox'][0]
+        face_loc = torch.cat([
+            o[:, :, :, :4*mbox_num].contiguous().view(o.size(0), -1)
+            for o in loc], dim=1)
+        face_conf = torch.cat([
+            o[:, :, :, :2*mbox_num].contiguous().view(o.size(0), -1)
+            for o in conf], dim=1)
+        # Test Phase
+        self.priors = self.init_priors(featuremap_size, image_size)
+        self.priors = self.priors.to(face_conf.device)
+        conf_preds = face_conf.view(
+            face_conf.size(0), -1, self.num_classes).softmax(dim=-1)
+        face_loc = face_loc.view(face_loc.size(0), -1, 4)
+        boxes = batched_decode(
+            face_loc, self.priors,
+            self.cfg["variance"]
+        )
+        scores = conf_preds.view(-1, self.priors.shape[0], 2)[:, :, 1:]
+        output = torch.cat((boxes, scores), dim=-1)
+        return output
