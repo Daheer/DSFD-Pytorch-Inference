4304358ef7994ac1c41122919a02c2bf2cef2db4
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-12-13 08:28:28 +0100

    Update detect.py

--- face_detection/dsfd/detect.py
+++ face_detection/dsfd/detect.py
@@ -64,7 +64,7 @@ class DSFDDetectorTensorRT(Detector):
     def _detect(self, x: torch.Tensor,) -> typing.List[np.ndarray]:
         x = x[:, [2, 1, 0], :, :]
         with torch.cuda.amp.autocast(enabled=self.fp16_inference):
-            boxes = self.net(
+            boxes = self.ssd(
                 x
             )
         return boxes
