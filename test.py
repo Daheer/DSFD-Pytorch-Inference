cb1ae1842c0c71f4d9c5e2e2e91cfec6adc0b108
Daheer <suhayrid6@gmail.com>
2023-12-14 18:42:46 +0000

    testing wrap

--- test.py
+++ test.py
@@ -2,11 +2,25 @@ import glob
 import os
 import cv2
 import time
+from numpy import array as np_array
 import face_detection
 
+TARGET_WIDTH = 640
+
+def resize_image(image):
+    original_width = image.shape[1]
+    original_height = image.shape[0]
+    aspect_ratio = original_width / original_height
+    target_height = int(TARGET_WIDTH / aspect_ratio)
+    resized_image = cv2.resize(image, (TARGET_WIDTH, target_height), interpolation=cv2.INTER_AREA)
+    padded_resized_image = cv2.copyMakeBorder(resized_image, 0, TARGET_WIDTH - target_height, 0, 0, cv2.BORDER_CONSTANT, value=0)
+
+    return padded_resized_image
 
 def draw_faces(im, bboxes):
+    h, w = im.shape[0], im.shape[1]
     for bbox in bboxes:
+        x0, y1, x1, y1 = bbox * np_array([w, h, w, h])
         x0, y0, x1, y1 = [int(_) for _ in bbox]
         cv2.rectangle(im, (x0, y0), (x1, y1), (0, 0, 255), 2)
 
@@ -17,19 +31,18 @@ if __name__ == "__main__":
     detector = face_detection.build_detector(
         "DSFDDetectorTensorRT",
         max_resolution=1080,
-        clip_boxes=True,
     )
     for impath in impaths:
         if impath.endswith("out.jpg"): continue
         im = cv2.imread(impath)
-        #===================
-        im = cv2.resize(im, (300, 300), interpolation = cv2.INTER_AREA)
+        resized_im = resize_image(im)
+        # im = cv2.resize(im, (300, 300), interpolation = cv2.INTER_AREA)
         print("Processing:", impath)
         t = time.time()
         dets = detector.detect(
             im[:, :, ::-1]
         )[:, :4]
-        import pdb; pdb.set_trace()
+        dets /= TARGET_WIDTH
         print(f"Detection time: {time.time()- t:.3f}")
         draw_faces(im, dets)
         imname = os.path.basename(impath).split(".")[0]
